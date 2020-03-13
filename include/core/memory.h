5558334a01c1271532b1c74a024c98e99bd33725
bcooperstl <coop999@gmail.com>
2020-03-13 14:24:50 -0500

    work in progress; changed memory to use pages

--- include/core/memory.h
+++ include/core/memory.h
@@ -2,26 +2,30 @@
 #define __MEMORY_H__
 
 #include <iostream>
+#include <map>
 
 #include "constants.h"
+#include "memory_page.h"
 
 class Memory
 {
 private:
-    int * m_ram; // Pointer to RAM. 
-    long m_size;
-    long m_max_used_address;
-    int get(long address, int * result);
+    int m_page_size;
+    std::map<long, MemoryPage *> m_pages; // Map of memory pages.  
+    int get(long address, long * result);
+    void calculatePageNumberOffset(long address, long & pageNumber, int & offset);
+    int getPage(long pageNumber, MemoryPage * page);
+    MemoryPage * createBlankPage();
+    int storePage(long pageNumber, MemoryPage * page);
 public:
-    Memory(long size=MEM_SIZE);
+    Memory(long page_size=MEM_PAGE_SIZE);
     Memory(const Memory & other);
     ~Memory();
-    int getImmediateMode(long address, int * result);
-    int getPositionMode(long address, int * result);
-    int get(long address, int mode, int * result);
-    int put(long address, int value);
-    long getSize();
-    long getMaxUsedAddress();
+    int getImmediateMode(long address, long * result);
+    int getPositionMode(long address, long * result);
+    int get(long address, int mode, long * result);
+    int put(long address, long value);
+    int getPageSize();
     void reset();
     void dump(std::ostream & out);
 };
